c7fdeca053c1cb9c16165fecdcb84f5c353a3afa
Eoin O'Connell <eoin@eoinoconnell.ie>
2016-04-07 23:15:05 +0100

    Removed pods and carthage from github

--- Pods/Target Support Files/Pods-DriftTests/Pods-DriftTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DriftTests : NSObject
-@end
-@implementation PodsDummy_Pods_DriftTests
-@end
--- Pods/Target Support Files/Pods-DriftTests/Pods-DriftTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_DriftTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DriftTestsVersionString[];
-
