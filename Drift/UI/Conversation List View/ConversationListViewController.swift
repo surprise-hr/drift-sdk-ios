6f8198d17400d28d55a4460b9b1d6c38cad54970
Adam Gammell <adamgammell@gmail.com>
2016-11-22 18:37:51 +0000

    Tidied up a few bits on the conversation list

--- Drift/UI/Conversation List View/ConversationListViewController.swift
+++ Drift/UI/Conversation List View/ConversationListViewController.swift
@@ -31,6 +31,8 @@ class ConversationListViewController: UIViewController {
         tableView.dataSource = self
         tableView.rowHeight = UITableViewAutomaticDimension
         tableView.estimatedRowHeight = 90
+        tableView.separatorColor = UIColor(white: 0, alpha: 0.05)
+        tableView.separatorInset = .zero
         tableView.register(UINib(nibName: "ConversationListTableViewCell", bundle:  Bundle(for: ConversationListTableViewCell.classForCoder())), forCellReuseIdentifier: "ConversationListTableViewCell")
         InboxManager.sharedInstance.addConversationSubscription(ConversationSubscription(delegate: self))
     }
