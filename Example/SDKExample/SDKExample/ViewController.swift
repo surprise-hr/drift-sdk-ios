6cf191b4de93b64820cd717d563284717b69fa7c
Eoin O’Connell <eoin@eoinoconnell.ie>
2017-05-29 13:36:59 +0100

    Added example Project

---
+++ Example/SDKExample/SDKExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SDKExample
+//
+//  Created by Eoin O'Connell on 29/05/2017.
+//  Copyright © 2017 drift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
