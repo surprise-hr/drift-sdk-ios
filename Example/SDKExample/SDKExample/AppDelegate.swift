bd6902c2d0358801f587b0be7f8933947b620e66
Brian McDonald <brianamcdon@gmail.com>
2017-06-30 16:55:38 +0100

    Format code in sample app

--- Example/SDKExample/SDKExample/AppDelegate.swift
+++ Example/SDKExample/SDKExample/AppDelegate.swift
@@ -13,8 +13,7 @@ import Drift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Drift.setup("teergsg9bb5d")
