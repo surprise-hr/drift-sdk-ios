839e643eb1f09fc0703c825eb8c623a5c8ccb9f8
Eoin O'Connell <eoin@8bytes.ie>
2017-07-12 08:47:57 +0100

    Layer removal (#20)
    * Update spec to 1.1.0
    
    * Removed embed id

--- Example/SDKExample/SDKExample/AppDelegate.swift
+++ Example/SDKExample/SDKExample/AppDelegate.swift
@@ -15,8 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        Drift.setup("teergsg9bb5d")
+
+        Drift.setup("")
         
 //        Drift.debugMode(true)
         Drift.registerUser("1237438", email: "eoin+app@8bytes.is")
