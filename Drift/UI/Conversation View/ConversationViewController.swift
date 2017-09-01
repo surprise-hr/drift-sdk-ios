60254e67ef3afbe3d9fe3e8ed1a43c37d6ad579e
Eoin O’Connell <eoin@eoinoconnell.ie>
2017-09-01 17:01:32 +0100

    Added robot and updates layout of welcome message a little bit

--- Drift/UI/Conversation View/ConversationViewController.swift
+++ Drift/UI/Conversation View/ConversationViewController.swift
@@ -177,15 +177,26 @@ class ConversationViewController: SLKTextViewController {
                 }
                 
                 if embed.userListMode == .custom {
-                    if let chosenTeamMember = embed.users.filter({embed.userListIds.contains($0.userId ?? -1)}).first, let avatarURLString = chosenTeamMember.avatarURL{
-                        if let avatarURL = URL(string: avatarURLString){
+                    if let teamMember = embed.users.filter({embed.userListIds.contains($0.userId ?? -1)}).first{
+                        
+                        if teamMember.bot {
+                            
+                            emptyState.avatarImageView.image = UIImage(named: "robot", in: Bundle(for: Drift.self), compatibleWith: nil)
+                            emptyState.avatarImageView.backgroundColor = DriftDataStore.sharedInstance.generateBackgroundColor()
+                            
+                        } else if let avatarURLString = teamMember.avatarURL, let avatarURL = URL(string: avatarURLString) {
                             emptyState.avatarImageView.af_setImage(withURL: avatarURL)
                         }
                     }
                 }else{
                     if embed.users.count > 0 {
                         let teamMember = embed.users[Int(arc4random_uniform(UInt32(embed.users.count)))]
-                        if let avatarURLString = teamMember.avatarURL, let avatarURL = URL(string: avatarURLString) {
+                        if teamMember.bot {
+                            
+                            emptyState.avatarImageView.image = UIImage(named: "robot", in: Bundle(for: Drift.self), compatibleWith: nil)
+                            emptyState.avatarImageView.backgroundColor = DriftDataStore.sharedInstance.generateBackgroundColor()
+                            
+                        } else if let avatarURLString = teamMember.avatarURL, let avatarURL = URL(string: avatarURLString) {
                             emptyState.avatarImageView.af_setImage(withURL: avatarURL)
                         }
                     }
